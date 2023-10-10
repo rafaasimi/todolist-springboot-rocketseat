1894e93e61598f93e6ac33a6974aca078fc8be68
Rafael Simionato <rafaasimi@hotmail.com>
2023-10-10 09:08:05 -0300

    Aula 2: Conexão com BD, geração de tabelas e tratamento de http status code

--- src/main/java/br/com/rafaelsimionato/todolist/user/UserModel.java
+++ src/main/java/br/com/rafaelsimionato/todolist/user/UserModel.java
@@ -1,9 +1,38 @@
 package br.com.rafaelsimionato.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+// import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/*
+ * @Data -> Colocar Getters e Setters para todas as variveis
+ * @Getters -> Colocar somente os métodos de Getters
+ * @Setters -> Colocar somente os métodos de Setters
+ */
+
+@Data
+@Entity(name="tb_users")
 public class UserModel {
   
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  // @Column(name="usuario")
+  @Column(unique = true)
   private String username;
   private String name;
   private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
